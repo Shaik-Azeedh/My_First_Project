1869295c5b1e290d9310691dda2e782dfc8a59b1
Shaik-Azeedh <153460338+Shaik-Azeedh@users.noreply.github.com>
2026-05-07 05:34:06 +0530

    Refactor Test04 to reduce duplicate prints and add loop
    Removed duplicate welcome messages and added a loop to print 'SYED NASREEN'.

--- Test04.java
+++ Test04.java
@@ -2,9 +2,7 @@ public class Test04 {
     public static void main(String[] args) {
      System.out.println("welcome to java world");
         System.out.println("welcome to java world");
-        System.out.println("welcome to java world");
-        System.out.println("welcome to java world");
-        System.out.println("welcome to java world");
-        System.out.println("welcome to java world");
+       for(i=0;i<20;i++) {
+           System.out.println("SYED NASREEN")
     }
 }
