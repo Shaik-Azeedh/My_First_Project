4dae86a40b4187b197e737994d023f54ac8c7faa
Shaik-Azeedh <153460338+Shaik-Azeedh@users.noreply.github.com>
2026-05-07 05:39:00 +0530

    Fix loop to iterate 50 times instead of 20

--- Test04.java
+++ Test04.java
@@ -2,7 +2,7 @@ public class Test04 {
     public static void main(String[] args) {
      System.out.println("welcome to java world");
         System.out.println("welcome to java world");
-       for(i=0;i<20;i++) {
+       for(int i=0;i<50;i++) {
            System.out.println("SYED NASREEN");}
     }
 }
